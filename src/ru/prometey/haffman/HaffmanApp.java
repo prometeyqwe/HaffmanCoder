e6329de26d1502f037346c83bb2a93f552de49b9
prometeyqwe <kirilllov.evgeny@mail.ru>
2018-10-28 23:54:31 +0300

    change HaffmanApp.java

--- src/ru/prometey/haffman/HaffmanApp.java
+++ src/ru/prometey/haffman/HaffmanApp.java
@@ -5,13 +5,10 @@ import java.util.Scanner;
 
 public class HaffmanApp {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
 
         HaffmanCoder haffmanCoder = new HaffmanCoder("file.txt");
         HaffmanDecoder haffmanDecoder = new HaffmanDecoder("1file.txt");
 
-        System.out.println();
         haffmanCoder.coder();
         haffmanDecoder.decod();
 
